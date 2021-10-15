8b497620391212026401a3789f0c505f23cfd9a4
Nikita Batov <batov@outlook.com>
2021-10-15 22:54:28 +0300

    Add -no-reboot key to stop on VM reset

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ cd "${CODE_PATH}"
 idf.py build
 cd build 
 esptool.py --chip esp32 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
-/opt/qemu/bin/qemu-system-xtensa -nographic -machine esp32 -drive file=flash_image.bin,if=mtd,format=raw
+/opt/qemu/bin/qemu-system-xtensa -nographic -no-reboot -machine esp32 -drive file=flash_image.bin,if=mtd,format=raw
