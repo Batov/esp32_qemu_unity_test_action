1621ef7e4e8874d1f22c227a49176170b3361fd0
Nikita Batov <batov@outlook.com>
2021-10-15 17:40:46 +0300

    Add action files

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+CODE_PATH="$1"
+. $IDF_PATH/export.sh
+cd "${CODE_PATH}"
+idf.py build
+cd build 
+esptool.py --chip esp32 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
+/opt/qemu/bin/qemu-system-xtensa -nographic -machine esp32 -drive file=flash_image.bin,if=mtd,format=raw
