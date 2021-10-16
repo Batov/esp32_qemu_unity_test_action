779ff0149b7b7fa405c59eb67a8b17ec8af31ea3
Nikita Batov <batov@outlook.com>
2021-10-16 16:21:38 +0300

    Move report.xml

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,3 +8,4 @@ cd build
 esptool.py --chip esp32 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
 /opt/qemu/bin/qemu-system-xtensa -nographic -no-reboot -machine esp32 -drive file=flash_image.bin,if=mtd,format=raw -serial file:output.log
 ruby /opt/Unity-2.5.2/auto/parse_output.rb output.log -xml
+mv report.xml /github/workspace
