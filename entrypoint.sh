7ee805bdf4cde04137de54c07940305577a2e9cd
Nikita Batov <nikitabatov@gmail.com>
2022-10-11 14:02:19 +0300

    Show output.log file

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,5 @@ cd build
 esptool.py --chip esp32 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
 /opt/qemu/bin/qemu-system-xtensa -nographic -no-reboot -machine esp32 -drive file=flash_image.bin,if=mtd,format=raw -serial file:output.log
 grep -q -E '[[:digit:]]+ Tests [[:digit:]]+ Failures [[:digit:]]+ Ignored' output.log && ruby /opt/Unity-2.5.2/auto/parse_output.rb -xml output.log || exit 1
+cat output.log
 mv report.xml /github/workspace
